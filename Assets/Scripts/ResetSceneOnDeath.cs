a0ca640cc4588eee2424902cff2aae3b4d975573
Filipower888 <filipefcoura888@gmail.com>
2023-05-27 19:09:09 -0300

    Update ResetSceneOnDeath.cs

--- Assets/Scripts/ResetSceneOnDeath.cs
+++ Assets/Scripts/ResetSceneOnDeath.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ResetSceneOnDeath : MonoBehaviour
 {
-    void OnCollisionEnter()
-    {
+    private Rigidbody2D _rigidbody;
+    private Collider2D _collider;
 
+    /*void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 45) \*Time.deltaTime);
+    }*/
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Scene");
+        }
     }
 }
+
